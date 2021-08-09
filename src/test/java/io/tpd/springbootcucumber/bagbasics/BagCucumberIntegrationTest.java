14566520689178ae94c309afeb2a09a2d33edea0
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-08-09 17:53:23 +0530

    first-6 commit

--- src/test/java/io/tpd/springbootcucumber/bagbasics/BagCucumberIntegrationTest.java
+++ src/test/java/io/tpd/springbootcucumber/bagbasics/BagCucumberIntegrationTest.java
@@ -5,8 +5,12 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/bagbasics",
-        plugin = {"pretty", "html:target/cucumber/bagbasics"},
-        extraGlue = "io.tpd.springbootcucumber.bagcommons")
+@CucumberOptions(
+		features = "src/test/resources/features/bagbasics",        
+        glue = {"io.tpd.springbootcucumber.bagbasics"},
+        plugin = ("json:target/cucumber-reports/cucumber-test-report.json"),
+        monochrome= true,
+        dryRun= false
+		)
 public class BagCucumberIntegrationTest {
 }
